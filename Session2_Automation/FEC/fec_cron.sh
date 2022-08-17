a2414a4e07205f13d2046e144128a945b972444e
ambreenchaudhri <49079761+ambreenchaudhri@users.noreply.github.com>
2022-08-17 10:33:40 -0500

    Update fec_cron.sh

--- Session2_Automation/FEC/fec_cron.sh
+++ Session2_Automation/FEC/fec_cron.sh
@@ -2,6 +2,6 @@
 
 source ~/.bash_profile
 module load python/anaconda3.6
-python ~/fellows_workshop_2022/Session2_Automation/FEC/1_fec_extract.py
+python ~/fellows_workshop_2022/Session2_Automation/FEC/1_fec_extract.py --start 2018 --end 2020
 echo "Cron Job is running on KLC Node 202."
 
